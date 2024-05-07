8319e80b1ff0969e1eaf38d87f61d243826df017
dinhtam1 <villadiwc123@gmail.com>
2024-05-07 15:51:40 +0700

    refactor/Moved hardcoded strings to constants file

--- Source/Backend/src/auth/authUtils.js
+++ Source/Backend/src/auth/authUtils.js
@@ -1,25 +1,28 @@
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const algorithms = {
+    HS256: 'HS256'
+}
 
 
 const createTokenPair = async (payload, publicKey, privateKey) => {
     try {
         const accessToken = await jwt.sign(payload, privateKey, {
-            algorithm: 'HS256',
-            expiresIn: '2 days'
+            algorithm: algorithms.HS256,
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES
         });
         const refreshToken = await jwt.sign(payload, privateKey, {
-            algorithm: 'HS256',
-            expiresIn: '7 days'
+            algorithm: algorithms.HS256,
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES
         });
 
-        jwt.verify(accessToken, privateKey, { algorithms: ['HS256'] }, (err, decode) => {
-            if (err) {
-                console.error('error verify ', err)
-            } else {
-                console.log('decode verify ', decode)
-            }
-        })
+        // jwt.verify(accessToken, privateKey, { algorithms: [algorithms.HS256] }, (err, decode) => {
+        //     if (err) {
+        //         console.error('error verify ', err)
+        //     } else {
+        //         console.log('decode verify ', decode)
+        //     }
+        // })
         return { accessToken, refreshToken }
     } catch (error) {
         return error;
--- Source/Backend/src/middlewares/authMiddleware.js
+++ Source/Backend/src/middlewares/authMiddleware.js
@@ -2,9 +2,14 @@ const keyTokenServices = require('../services/keyTokenServices.js');
 const statusCode = require('../constant/appNumber.js')
 const Type = require('../constant/appRequestType.js')
 const jwt = require('jsonwebtoken');
+
+const appString = require('../constant/appString.js')
 const HEADER = {
     CLIENT_ID: 'x-client-id',
     AUTHORIZATION: 'authorization',
+}
+const algorithm = {
+    HS256 : 'HS256',
 }
 
 const verifyToken = async (req, res, next) => {
@@ -13,9 +18,9 @@ const verifyToken = async (req, res, next) => {
     try {
         const userId = Number(req.headers[HEADER.CLIENT_ID]);
         if (!userId) {
-            return res.status(400).json({
+            return res.status(statusCode.BAD_REQUEST).json({
                 statusCode: statusCode.BAD_REQUEST,
-                message: 'User not found',
+                message: appString.USER_NOT_FOUND,
                 data,
                 requestType
             });
@@ -23,9 +28,9 @@ const verifyToken = async (req, res, next) => {
 
         const keyStore = await keyTokenServices.getKeyTokenByUserId(userId);
         if (!keyStore) {
-            return res.status(400).json({
+            return res.status(statusCode.BAD_REQUEST).json({
                 statusCode: statusCode.BAD_REQUEST,
-                message: 'KeyStore not found',
+                message: appString.KEYSTORE_NOT_FOUND,
                 data,
                 requestType
             });
@@ -33,41 +38,40 @@ const verifyToken = async (req, res, next) => {
 
         const accessToken = req.headers[HEADER.AUTHORIZATION];
         if (!accessToken) {
-            return res.status(400).json({
+            return res.status(statusCode.BAD_REQUEST).json({
                 statusCode: statusCode.BAD_REQUEST,
-                message: 'You are not authenticated',
+                message: appString.NOT_AUTHENTICATED,
                 data,
                 requestType
             });
         }
 
-        jwt.verify(accessToken, keyStore.privateKey, { algorithms: ['HS256'] }, (err, decodeUser) => {
+        jwt.verify(accessToken, keyStore.privateKey, { algorithms: [algorithm.HS256] }, (err, decodeUser) => {
             if (err) {
                 console.log(err)
-                return res.status(500).json({
+                return res.status(statusCode.INTERNAL_SERVER_ERROR).json({
                     statusCode: statusCode.INTERNAL_SERVER_ERROR,
-                    message: 'Error verifying token',
+                    message: appString.ERROR_VERIFYING_TOKEN,
                     data,
                     requestType
                 });
             }
 
             if (userId !== decodeUser.userId) {
-                return res.status(200).json({
+                return res.status(statusCode.SUCCESS).json({
                     statusCode: statusCode.SUCCESS,
-                    message: 'Invalid User',
+                    message: appString.INVALID_USER,
                     data,
                     requestType
                 });
             }
-
             delete decodeUser.iat;
             delete decodeUser.exp;
             req.user = decodeUser;
             next();
         });
     } catch (error) {
-        return res.status(500).json({
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).json({
             statusCode: statusCode.INTERNAL_SERVER_ERROR,
             message: error.message,
             requestType
