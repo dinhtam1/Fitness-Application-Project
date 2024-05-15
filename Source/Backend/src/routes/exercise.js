91f0aa615c0135cca0d612a7c4530fa9add0f03c
dinhtam1 <villadiwc123@gmail.com>
2024-05-15 20:09:40 +0700

    feat/FITNESS-35/Create API view result

--- Source/Backend/src/routes/exercise.js
+++ Source/Backend/src/routes/exercise.js
@@ -4,7 +4,8 @@ const exerciseController = require('../controllers/exerciseController');
 const authValidation = require('../validation/authValidation');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-exerciseRouter.get('/category', authMiddleware.verifyToken, exerciseController.getCategory)
 exerciseRouter.get('/', authMiddleware.verifyToken, exerciseController.getExercise)
+exerciseRouter.get('/category', authMiddleware.verifyToken, exerciseController.getCategory)
+exerciseRouter.get('/result/:id', authMiddleware.verifyToken, exerciseController.getResultExercise)
 exerciseRouter.get('/:id', authMiddleware.verifyToken, exerciseController.getDetailExercise)
 module.exports = exerciseRouter;
