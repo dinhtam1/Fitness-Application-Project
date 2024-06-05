eb0caedf8f9d460fd790aa5332b3d69bfe0ed7d4
dinhtam1 <villadiwc123@gmail.com>
2024-06-05 20:58:05 +0700

    feat/FITNESS-51/Create API view dashboard by time period

--- Source/Backend/src/controllers/dashboardController.js
+++ Source/Backend/src/controllers/dashboardController.js
@@ -2,13 +2,43 @@ const dashboardServices = require('../services/dashboardServices.js');
 const statusCode = require('../constant/appNumber.js')
 const Type = require('../constant/appRequestType.js')
 const appString = require('../constant/appString.js')
+const moment = require('moment');
 
+moment.updateLocale('en', {
+    // your custom configuration here
+});
+const PERIOD = {
+    WEEK: 'week',
+    MONTH: 'month',
+    DAY: 'day'
+}
+const PERIOD_TYPE = {
+    WEEK: 'isoweek',
+    MONTH: 'month',
+    DAY: 'DD/MM/YYYY'
+}
 const getDashboard = async (req, res) => {
     const requestType = Type.GET_DASHBOARD;
     var data = null;
     try {
         const userId = req.user.userId;
-        const dashboard = await dashboardServices.getDashboard(userId)
+        let startDate, endDate;
+        const { period, start, end } = req.query;
+        if (period === PERIOD.WEEK) {
+            startDate = moment().startOf(PERIOD_TYPE.WEEK).add(1, PERIOD.DAY).toDate();
+            endDate = moment().endOf(PERIOD_TYPE.WEEK).add(1, PERIOD.DAY).toDate();
+            returnDayNames = true;
+        } else if (period === PERIOD.MONTH) {
+            startDate = moment().startOf(PERIOD_TYPE.MONTH).toDate();
+            endDate = moment().endOf(PERIOD_TYPE.MONTH).toDate();
+        } else if (start && end) {
+            startDate = moment(start, PERIOD_TYPE.DAY).toDate();
+            endDate = moment(end, PERIOD_TYPE.DAY).toDate();
+        } else {
+            startDate = undefined;
+            endDate = undefined
+        }
+        const dashboard = await dashboardServices.getDashboard(userId, startDate, endDate)
         if (!dashboard) {
             return res.status(statusCode.SUCCESS).json({
                 statusCode: statusCode.FAIL,
@@ -40,7 +70,7 @@ const updateDashboard = async (req, res) => {
     var data = null;
     try {
         const updatedDashboard = await dashboardServices.updateDashboard(req.user, req.body)
-        if(!updatedDashboard) {
+        if (!updatedDashboard) {
             return res.status(statusCode.SUCCESS).json({
                 statusCode: statusCode.FAIL,
                 message: appString.UPDATE_DASHBOARD_FAILED,
@@ -49,7 +79,7 @@ const updateDashboard = async (req, res) => {
             });
         }
         return res.status(statusCode.SUCCESS).json({
-            statusCode : statusCode.SUCCESS,
+            statusCode: statusCode.SUCCESS,
             message: appString.UPDATE_DASHBOARD_SUCCESSFUL,
             data: updatedDashboard,
             requestType
